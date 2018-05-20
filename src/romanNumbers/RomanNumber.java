baa0c2f069bcbe9c045c28f4c05f2a0bbab188c3
edmundbuchbinder <edmundbuchbinder@BK7N00047896.bku.db.de>
2018-05-20 14:03:50 +0200

    first version

---
+++ src/romanNumbers/RomanNumber.java
@@ -0,0 +1,39 @@
+package romanNumbers;
+import java.util.*;
+
+public class RomanNumber {	
+	
+	
+	
+	
+	public RomanNumber() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int switchRomanToInt (String input) {
+		int result;
+		
+		/* den Input von links nach rechts Buchstabenweise durchgehen und aufaddieren
+		 * Die Umrechnung erfolgt durch die Subtraktionsregel 
+		 * 
+		 * 
+		 * 
+		 * 
+		 * 		 */
+				return 1;
+		
+		
+	}
+	
+	 public static void main(String[] args) {
+		 	System.out.println("Geben Sie die Laenge L ein:");
+		 	Scanner sc = new Scanner(System.in);		 	
+		 	String input = sc.nextLine();  
+	        
+	        
+	        
+	        System.out.println("Sie haben den folgenden String eingegeben " + input);
+	 }
+
+}
